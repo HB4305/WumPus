f3533fbc002d8e52dad03e4db93d57218e3278f8
thekeivn <201560484+thekeivn16@users.noreply.github.com>
2025-08-03 17:07:12 +0700

    environment.py

--- wumpus/environment.py
+++ wumpus/environment.py
@@ -1,3 +1,113 @@
+# import random
+# from .utils import get_neighbors
+
+# class Cell:
+#     def __init__(self):
+#         self.wumpus = False
+#         self.pit = False
+#         self.gold = False
+#         self.visited = False
+#         self.stench = False
+#         self.breeze = False
+#         self.glitter = False
+
+# class Environment:
+#     def __init__(self, size=4, k=2, pit_prob=0.2):
+#         self.size = size
+#         self.grid = [[Cell() for _ in range(size)] for _ in range(size)]
+#         self.wumpus_positions = set()
+#         self.agent_pos = (0, 0)
+#         self.agent_dir = "EAST"
+#         self.arrow_available = True
+#         self.scream = False
+
+#         self.place_entities(k, pit_prob)
+
+#     def in_bounds(self, x, y):
+#         return 0 <= x < self.size and 0 <= y < self.size
+
+#     def place_entities(self, k, pit_prob):
+#         positions = [(i, j) for i in range(self.size) for j in range(self.size) if (i, j) != (0, 0)]
+#         random.shuffle(positions)
+
+#         # place k wumpus
+#         for _ in range(k):
+#             x, y = positions.pop()
+#             self.grid[x][y].wumpus = True
+#             self.wumpus_positions.add((x, y))
+#             for nx, ny in get_neighbors((x, y), self.size):
+#                 self.grid[nx][ny].stench = True
+
+#         # place pits
+#         for i in range(self.size):
+#             for j in range(self.size):
+#                 if (i, j) != (0, 0) and random.random() < pit_prob and not self.grid[i][j].wumpus:
+#                     self.grid[i][j].pit = True
+#                     for nx, ny in get_neighbors((i, j), self.size):
+#                         self.grid[nx][ny].breeze = True
+
+#         # place gold, can be at (0,0)
+#         gold_candidates = [
+#             (i, j) for i in range(self.size) for j in range(self.size)
+#             if not self.grid[i][j].wumpus and not self.grid[i][j].pit
+#         ]
+#         gold_x, gold_y = random.choice(gold_candidates)
+#         self.grid[gold_x][gold_y].gold = True
+#         self.grid[gold_x][gold_y].glitter = True
+
+#     def get_percepts(self, x=None, y=None):
+#         if x is None or y is None:
+#             x, y = self.agent_pos
+#         cell = self.grid[x][y]
+#         return {
+#             "stench": cell.stench,
+#             "breeze": cell.breeze,
+#             "glitter": cell.glitter,
+#             "bump": False,
+#             "scream": self.scream
+#         }
+
+#     def move_agent(self, new_x, new_y):
+#         if not self.in_bounds(new_x, new_y):
+#             return {"bump": True, **self.get_percepts()}
+#         else:
+#             self.agent_pos = (new_x, new_y)
+#             return {"bump": False, **self.get_percepts()}
+
+#     def rotate_agent(self, direction):
+#         dirs = ["NORTH", "EAST", "SOUTH", "WEST"]
+#         idx = dirs.index(self.agent_dir)
+#         if direction == "LEFT":
+#             self.agent_dir = dirs[(idx - 1) % 4]
+#         elif direction == "RIGHT":
+#             self.agent_dir = dirs[(idx + 1) % 4]
+
+#     def shoot_arrow(self):
+#         if not self.arrow_available:
+#             return {"scream": False, **self.get_percepts()}
+#         self.arrow_available = False
+#         x, y = self.agent_pos
+#         dx, dy = {
+#             "NORTH": (0, 1),
+#             "EAST": (1, 0),
+#             "SOUTH": (0, -1),
+#             "WEST": (-1, 0)
+#         }[self.agent_dir]
+
+#         scream = False
+#         while self.in_bounds(x + dx, y + dy):
+#             x += dx
+#             y += dy
+#             if (x, y) in self.wumpus_positions:
+#                 self.wumpus_positions.remove((x, y))
+#                 self.grid[x][y].wumpus = False
+#                 # remove stench
+#                 for nx, ny in get_neighbors((x, y), self.size):
+#                     self.grid[nx][ny].stench = False
+#                 scream = True
+#                 break
+#         self.scream = scream
+#         return {"scream": scream, **self.get_percepts()}
 import random
 from .utils import get_neighbors
 
@@ -15,18 +125,30 @@ class Environment:
     def __init__(self, size=4, k=2, pit_prob=0.2):
         self.size = size
         self.grid = [[Cell() for _ in range(size)] for _ in range(size)]
+        self.wumpus_positions = set()
+        self.agent_pos = (0, 0)
+        self.agent_dir = "EAST"
+        self.arrow_available = True
+        self.scream = False
+
         self.place_entities(k, pit_prob)
+
+    def in_bounds(self, x, y):
+        return 0 <= x < self.size and 0 <= y < self.size
 
     def place_entities(self, k, pit_prob):
         positions = [(i, j) for i in range(self.size) for j in range(self.size) if (i, j) != (0, 0)]
         random.shuffle(positions)
 
+        # place k wumpus
         for _ in range(k):
             x, y = positions.pop()
             self.grid[x][y].wumpus = True
+            self.wumpus_positions.add((x, y))
             for nx, ny in get_neighbors((x, y), self.size):
                 self.grid[nx][ny].stench = True
 
+        # place pits
         for i in range(self.size):
             for j in range(self.size):
                 if (i, j) != (0, 0) and random.random() < pit_prob and not self.grid[i][j].wumpus:
@@ -34,16 +156,66 @@ class Environment:
                     for nx, ny in get_neighbors((i, j), self.size):
                         self.grid[nx][ny].breeze = True
 
-        gold_x, gold_y = positions.pop()
+        # place gold, can be at (0,0)
+        gold_candidates = [
+            (i, j) for i in range(self.size) for j in range(self.size)
+            if not self.grid[i][j].wumpus and not self.grid[i][j].pit
+        ]
+        gold_x, gold_y = random.choice(gold_candidates)
         self.grid[gold_x][gold_y].gold = True
         self.grid[gold_x][gold_y].glitter = True
 
-    def get_percepts(self, x, y):
+    def get_percepts(self, x=None, y=None):
+        if x is None or y is None:
+            x, y = self.agent_pos
         cell = self.grid[x][y]
         return {
             "stench": cell.stench,
             "breeze": cell.breeze,
             "glitter": cell.glitter,
             "bump": False,
-            "scream": False
+            "scream": self.scream
         }
+
+    def move_agent(self, new_x, new_y):
+        if not self.in_bounds(new_x, new_y):
+            return {"bump": True, **self.get_percepts()}
+        else:
+            self.agent_pos = (new_x, new_y)
+            return {"bump": False, **self.get_percepts()}
+
+    def rotate_agent(self, direction):
+        dirs = ["NORTH", "EAST", "SOUTH", "WEST"]
+        idx = dirs.index(self.agent_dir)
+        if direction == "LEFT":
+            self.agent_dir = dirs[(idx - 1) % 4]
+        elif direction == "RIGHT":
+            self.agent_dir = dirs[(idx + 1) % 4]
+
+    def shoot_arrow(self):
+        if not self.arrow_available:
+            return {"scream": False, **self.get_percepts()}
+        self.arrow_available = False
+        x, y = self.agent_pos
+        dx, dy = {
+            "NORTH": (0, 1),
+            "EAST": (1, 0),
+            "SOUTH": (0, -1),
+            "WEST": (-1, 0)
+        }[self.agent_dir]
+
+        scream = False
+        while self.in_bounds(x + dx, y + dy):
+            x += dx
+            y += dy
+            if (x, y) in self.wumpus_positions:
+                self.wumpus_positions.remove((x, y))
+                self.grid[x][y].wumpus = False
+                # remove stench
+                for nx, ny in get_neighbors((x, y), self.size):
+                    self.grid[nx][ny].stench = False
+                scream = True
+                break
+        self.scream = scream
+        return {"scream": scream, **self.get_percepts()}
+
--- wumpus/inference.py
+++ wumpus/inference.py
@@ -8,16 +8,19 @@
 #         self.unknown = set((i, j) for i in range(size) for j in range(size))
 #         self.percepts = dict()
 
-#     def get_possible_wumpus(self):
-#         # Trả về danh sách ô có khả năng chứa Wumpus
-#         return list(self.possible_wumpus_cells)
-
-#     def is_wumpus_certain(self, pos):
-#         # Trả về True nếu inference chắc chắn Wumpus ở ô này
-#         return pos in self.confirmed_wumpus_cells
+#         # ✅ Thêm 2 thuộc tính này để tránh lỗi AttributeError
+#         self.possible_wumpus_cells = set()
+#         self.confirmed_wumpus_cells = set()
+#         # ✅ Đánh dấu (0, 0) là safe ngay từ đầu
+#         self.safe.add((0, 0))
+#         self.visited = set()
+#         self.safe_cells = set()
 
 #     def update_knowledge(self, position, percept):
 #         self.percepts[position] = percept
+#         self.visited.add(position)
+#         self.safe_cells.add(position)  # ✅ phải có dòng này!
+
 #         if not percept["breeze"] and not percept["stench"]:
 #             for n in get_neighbors(position, self.size):
 #                 self.safe.add(n)
@@ -25,69 +28,138 @@
 #         else:
 #             self.unknown.add(position)
 
+#         # ✅ Cập nhật các ô nghi ngờ Wumpus nếu có stench
+#         if percept["stench"]:
+#             neighbors = get_neighbors(position, self.size)
+#             for n in neighbors:
+#                 if n not in self.safe:
+#                     self.possible_wumpus_cells.add(n)
+
+
+#         # ✅ Suy luận đơn giản: nếu chỉ còn đúng 1 ô nghi Wumpus và nó nằm trong tất cả stench, ta xác nhận nó
+#         self.infer_wumpus_certainty()
+
+#     def infer_wumpus_certainty(self):
+#         stench_positions = [pos for pos, p in self.percepts.items() if p["stench"]]
+#         if not stench_positions:
+#             return
+
+#         # Giao nhau các ô lân cận của tất cả vị trí có stench
+#         possible = set(get_neighbors(stench_positions[0], self.size))
+#         for pos in stench_positions[1:]:
+#             possible &= set(get_neighbors(pos, self.size))
+
+#         # Loại trừ các ô đã xác định an toàn
+#         possible -= self.safe
+
+#         if len(possible) == 1:
+#             self.confirmed_wumpus_cells = possible
+
+#     def get_possible_wumpus(self):
+#         return list(self.possible_wumpus_cells)
+
+#     def is_wumpus_certain(self, pos):
+#         return pos in self.confirmed_wumpus_cells
+
 #     def is_safe(self, pos):
 #         return pos in self.safe
 from .utils import get_neighbors
+from collections import defaultdict
 
 class InferenceEngine:
     def __init__(self, size):
         self.size = size
-        self.safe = set()
-        self.unsafe = set()
-        self.unknown = set((i, j) for i in range(size) for j in range(size))
-        self.percepts = dict()
-
-        # ✅ Thêm 2 thuộc tính này để tránh lỗi AttributeError
-        self.possible_wumpus_cells = set()
-        self.confirmed_wumpus_cells = set()
-        # ✅ Đánh dấu (0, 0) là safe ngay từ đầu
-        self.safe.add((0, 0))
         self.visited = set()
-        self.safe_cells = set()
+        self.safe = set([(0, 0)])
+        self.unknown = set((i, j) for i in range(size) for j in range(size))
+        self.knowledge = defaultdict(lambda: {
+            "possible_pit": False,
+            "possible_wumpus": False,
+            "confirmed_pit": False,
+            "confirmed_wumpus": False,
+            "safe": False
+        })
+        self.percepts = {}
 
-    def update_knowledge(self, position, percept):
-        self.percepts[position] = percept
-        self.visited.add(position)
-        self.safe_cells.add(position)  # ✅ phải có dòng này!
+    def update_knowledge(self, pos, percept):
+        self.percepts[pos] = percept
+        self.visited.add(pos)
+        self.safe.add(pos)
+        self.unknown.discard(pos)
+        self.knowledge[pos]["safe"] = True
 
+        neighbors = get_neighbors(pos, self.size)
+
+        # Nếu không có breeze hoặc stench → tất cả neighbors an toàn
         if not percept["breeze"] and not percept["stench"]:
-            for n in get_neighbors(position, self.size):
-                self.safe.add(n)
-                self.unknown.discard(n)
-        else:
-            self.unknown.add(position)
-
-        # ✅ Cập nhật các ô nghi ngờ Wumpus nếu có stench
-        if percept["stench"]:
-            neighbors = get_neighbors(position, self.size)
             for n in neighbors:
-                if n not in self.safe:
-                    self.possible_wumpus_cells.add(n)
+                self.safe.add(n)
+                self.knowledge[n]["safe"] = True
+                self.knowledge[n]["possible_pit"] = False
+                self.knowledge[n]["possible_wumpus"] = False
+                self.unknown.discard(n)
 
-        # ✅ Suy luận đơn giản: nếu chỉ còn đúng 1 ô nghi Wumpus và nó nằm trong tất cả stench, ta xác nhận nó
-        self.infer_wumpus_certainty()
+        # Nếu có breeze → đánh dấu neighbors là "có thể có pit"
+        if percept["breeze"]:
+            for n in neighbors:
+                if n not in self.safe and n not in self.visited:
+                    self.knowledge[n]["possible_pit"] = True
 
-    def infer_wumpus_certainty(self):
+        # Nếu có stench → đánh dấu neighbors là "có thể có wumpus"
+        if percept["stench"]:
+            for n in neighbors:
+                if n not in self.safe and n not in self.visited:
+                    self.knowledge[n]["possible_wumpus"] = True
+
+        # Nếu đã đến ô không có stench nhưng trước đó nghi ngờ Wumpus ở neighbor → loại bỏ
+        if not percept["stench"]:
+            for n in neighbors:
+                self.knowledge[n]["possible_wumpus"] = False
+
+        if not percept["breeze"]:
+            for n in neighbors:
+                self.knowledge[n]["possible_pit"] = False
+
+        # Sau đó cố gắng infer chắc chắn pit/wumpus từ giao nhau
+        self.infer_certain_threats()
+
+    def infer_certain_threats(self):
+        # Tìm tất cả vị trí có stench
         stench_positions = [pos for pos, p in self.percepts.items() if p["stench"]]
-        if not stench_positions:
-            return
+        if stench_positions:
+            common_wumpus = set(get_neighbors(stench_positions[0], self.size))
+            for pos in stench_positions[1:]:
+                common_wumpus &= set(get_neighbors(pos, self.size))
 
-        # Giao nhau các ô lân cận của tất cả vị trí có stench
-        possible = set(get_neighbors(stench_positions[0], self.size))
-        for pos in stench_positions[1:]:
-            possible &= set(get_neighbors(pos, self.size))
+            # Loại bỏ các ô đã an toàn
+            common_wumpus = {pos for pos in common_wumpus if not self.knowledge[pos]["safe"]}
+            if len(common_wumpus) == 1:
+                pos = list(common_wumpus)[0]
+                self.knowledge[pos]["confirmed_wumpus"] = True
 
-        # Loại trừ các ô đã xác định an toàn
-        possible -= self.safe
+        # Tương tự cho pit
+        breeze_positions = [pos for pos, p in self.percepts.items() if p["breeze"]]
+        if breeze_positions:
+            common_pit = set(get_neighbors(breeze_positions[0], self.size))
+            for pos in breeze_positions[1:]:
+                common_pit &= set(get_neighbors(pos, self.size))
 
-        if len(possible) == 1:
-            self.confirmed_wumpus_cells = possible
-
-    def get_possible_wumpus(self):
-        return list(self.possible_wumpus_cells)
-
-    def is_wumpus_certain(self, pos):
-        return pos in self.confirmed_wumpus_cells
+            common_pit = {pos for pos in common_pit if not self.knowledge[pos]["safe"]}
+            if len(common_pit) == 1:
+                pos = list(common_pit)[0]
+                self.knowledge[pos]["confirmed_pit"] = True
 
     def is_safe(self, pos):
-        return pos in self.safe
+        return self.knowledge[pos]["safe"] and not self.knowledge[pos]["confirmed_pit"] and not self.knowledge[pos]["confirmed_wumpus"]
+
+    def is_possible_pit(self, pos):
+        return self.knowledge[pos]["possible_pit"]
+
+    def is_possible_wumpus(self, pos):
+        return self.knowledge[pos]["possible_wumpus"]
+
+    def is_confirmed_pit(self, pos):
+        return self.knowledge[pos]["confirmed_pit"]
+
+    def is_confirmed_wumpus(self, pos):
+        return self.knowledge[pos]["confirmed_wumpus"]
